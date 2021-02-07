fc3ccaa1c14222ff5963a244cd34f922d25f6c14
Logan Pratico <logan@MacBook-Pro.local>
2021-02-07 13:14:02 -0500

    Bug fixes, connected to RMarkdown File

--- RScripts/article_scan.R
+++ RScripts/article_scan.R
@@ -1,46 +1,67 @@
 
- category <- commandArgs[length(commandArgs)]
- commandArgs <- head(commandArgs,-1)
- keywords <- commandArgs
+category <- commandArgs[length(commandArgs)]
+commandArgs <- head(commandArgs, -1)
+keywords <- commandArgs
+
+dir.create(
+  path = here("output", as.character(Sys.Date()), category), recursive = TRUE,
+  showWarnings = FALSE
+)
+
+errorFile <- here("output", Sys.Date(), category, "errors.txt")
+cat("ERRORS and WARNINGS:\n------------\n", file = errorFile, sep = "\n")
+
+
 
 
 walk(urls, function(url) {
   message(url)
-  items <- tidyfeed(url) %>%
-    select(-starts_with("feed")) %>%
-    select(contains("title"), contains("link"), contains("content"), contains("description")) %>%
-    set_colnames(c("title", "url", "description"))
 
- # print(items)
-
-  walk(keywords, function(keyword) {
-    dir.create(
-      path = here("output", as.character(Sys.Date()), category), recursive = TRUE,
-      showWarnings = FALSE
-    )
-
-    filename <- here("output", Sys.Date(), category, paste0(keyword, ".csv"))
-    keywords_list <-
-      items %>%
-      filter(
-        str_detect(tolower(title), tolower(keyword)) |
-          str_detect(tolower(description), tolower(keyword))
-      ) %>%
-      {
-        if (grepl("bbc", url) || grepl("nytimes", url)) top_n(., 1) else top_n(., 3)
-      }
+  result <- tryCatch(
+    {
+      items <- tidyfeed(url) %>%
+        select(-starts_with("feed")) %>%
+        select(contains("title"), contains("link"), contains("content"), contains("description")) %>%
+        set_colnames(c("title", "url", "description"))
+      walk(keywords, function(keyword) {
+        filename <- here("output", Sys.Date(), category, paste0(keyword, ".csv"))
+        keywords_list <-
+          items %>%
+          filter(
+            str_detect(tolower(title), tolower(keyword)) |
+              str_detect(tolower(description), tolower(keyword))
+          ) %>%
+          top_n(1)
+        # {
+        #   if (grepl("bbc", url) || grepl("nytimes", url)) top_n(., 1) else top_n(., 1)
+        # }
 
 
-    if (nrow(keywords_list) > 0) {
-      write.table(keywords_list,
-        file = filename,
-        append = file.exists(filename),
-        sep = ",",
-        row.names = FALSE,
-        col.names = !(file.exists(filename))
+        if (nrow(keywords_list) > 0) {
+          write.table(keywords_list,
+            file = filename,
+            append = file.exists(filename),
+            sep = ",",
+            row.names = FALSE,
+            col.names = !(file.exists(filename))
+          )
+        }
+      })
+    },
+    warning = function(w) {
+      cat(paste("WARNING:\n", w, "\nURL\n", url, "\n---------\n\n"),
+        file = errorFile, append = TRUE
       )
+    },
+    error = function(e) {
+      cat(paste("ERROR:\n", e, "\nURL\n", url, "\n---------\n\n"),
+        file = errorFile, append = TRUE
+      )
+    },
+    finally = {
+      # Do Nothing
     }
-  })
+  )
 })
 
 warnings()
--- RScripts/main.R
+++ RScripts/main.R
@@ -5,6 +5,8 @@ library(stringr)
 library(purrr)
 library(here)
 library(tidyRSS)
+library(rmarkdown)
+library(formatR)
 
 urls <- c(
   "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml", # NYT
@@ -23,31 +25,35 @@ urls <- c(
   "http://feeds.bbci.co.uk/news/technology/rss.xml",
   "https://news.google.com/rss/search?q=when:24h+allinurl:jacobinmag.com&hl=en-US&gl=US&ceid=US:en", # Jacobin
   "https://news.google.com/rss/search?q=when:24h+allinurl:time.com&hl=en-US&gl=US&ceid=US:en", # TIME
-  "https://news.google.com/rss/search?q=when:24h+allinurl:cnn.com&hl=en-US&gl=US&ceid=US:en",# CNN
-  "https://news.google.com/rss/search?q=when:24h+allinurl:wired.com&hl=en-US&gl=US&ceid=US:en", #WIRED
-  "https://news.google.com/rss/search?q=when:24h+allinurl:independent.co.uk&hl=en-US&gl=US&ceid=US:en" #Independent
-  
+  "https://news.google.com/rss/search?q=when:24h+allinurl:cnn.com&hl=en-US&gl=US&ceid=US:en", # CNN
+  "https://news.google.com/rss/search?q=when:24h+allinurl:wired.com&hl=en-US&gl=US&ceid=US:en", # WIRED
+  "https://news.google.com/rss/search?q=when:24h+allinurl:independent.co.uk&hl=en-US&gl=US&ceid=US:en" # Independent
 )
+
+
+
+
 
 
 vec1 <- c("COVID", "virus", "pandemic", "vaccine", "COVID_News")
 vec2 <- c(" AI ", "Artificial Intelligence", "Neural Networks", "Deep Learning", "AI_News")
 vec3 <- c("quantum computing", "quantum computers", "quantum internet", "Quantum_News")
-vec4 <- c("Clean Energy", "Solar Power", "wind power", "fossil fuels", "coal",
-          "green new deal", "Energy_News")
+vec4 <- c(
+  "Clean Energy", "Solar Power", "wind power", "fossil fuels", "coal",
+  "green new deal", "Energy_News"
+)
 vec5 <- c("QAnon", "Fake news", "misinformation", "disinformation", "Disinformation_News")
 vec6 <- c("Biden", "Trump", "Congress", "Impeachment", "Supreme Court", "Political_News")
 
 keyWordsList <- list(vec1, vec2, vec3, vec4, vec5, vec6)
 
 i <- 1
-while(i < length(keyWordsList)){
+while (i < length(keyWordsList)) {
   commandArgs <- unlist(keyWordsList[i])
   source(here("RScripts", "article_scan.R"))
   i <- i + 1
-  
 }
 
-#source(here("RScripts", "News.rmd"))
+rmarkdown::render(here("RScripts", "News.Rmd"), "pdf_document", here("output", Sys.Date(), "News"))
 
-
+# source(here("RScripts", "News.rmd"))
